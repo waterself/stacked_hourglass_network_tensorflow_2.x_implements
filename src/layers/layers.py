9cb79407abb9ffab2da6b2597d10170e600a0359
waterself <kimychsw@gmail.com>
2023-08-30 23:52:46 +0900

    process: add blocks

--- src/layers/layers.py
+++ src/layers/layers.py
@@ -1,5 +1,6 @@
 ﻿import tensorflow as tf
 from tensorflow import keras
+from keras.layers import Layer
 
 
 # TODO: Using Conv, Max Pooling To DownSampling
@@ -21,8 +22,34 @@ Batch Normalization
 #         w_init = tf.random_normal_initializer()
 
 class Residual(keras.layers.Layer):
-    def __init__(self, input_dim, output_dim):
-        conv1 = keras.layers.Conv2D(input_dim, )
+    def __init__(self, kernel_size, filters):
+        super(Residual, self).__init__()
+        self.relu = tf.nn.relu()
+        self.batchNorm1 = keras.layers.BatchNormalization()
+        self.conv1 = keras.layers.Conv2D( )
+        self.batchNorm2 = keras.layers.BatchNormalization()
+        self.conv2 = keras.layers.Conv2D( )
+        self.batchNorm3 = keras.layers.BatchNormalization()
+        self.conv3 = keras.layers.Conv2D( )
+
+
+    def call(self, inputs, training=None, mask=None):
+        
+        x = self.batchNorm1(inputs)
+        x = self.relu(x)
+        x = self.conv1(x)
+
+        x = self.batchNorm2(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        x = self.batchNorm3(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        
+        return super().call(inputs, training, mask)
+
+
 
 
 
