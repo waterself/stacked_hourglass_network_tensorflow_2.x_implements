5c255e0a184fa59e5486cd319bca88703ab7d54c
waterself98 <kimychsw@gmail.com>
2023-09-24 16:44:59 +0900

    add: data generator

---
+++ src/dataset/MPII_dataset.py
@@ -0,0 +1,12 @@
+﻿import json
+import tensorflow as tf
+
+target_json_path = "./MPII/mpii_human_pose_v1_u12_1.json"
+
+annotation_json = json.loads(target_json_path)
+
+print(annotation_json)
+
+class MPII_dataset(tf.data.Dataset):
+    def _generator(num_samples):
+        
